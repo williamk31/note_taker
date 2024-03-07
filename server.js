339e3e09b14208789571aac10875ee576444d61a
goofy foot <williamk31@gmail.com>
2024-03-07 14:39:15 -0700

    added PUSH request to server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = 3001
+const notes = require('./db/db.json')
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // middleware to serve static files
 app.use(express.static('public'));
@@ -10,6 +14,45 @@ app.use(express.static('public'));
 // GET request for notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+// GET api request for db.json
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
+});
+
+// POST request for new notes
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body
+    if(title && text) {
+        const newNote = {
+            title,
+            text,
+            // id: uuid(),
+        }
+        // const noteString = JSON.stringify(newNote);
+        fs.readFile('db/db.json', function (err, data){
+            if (err) {
+                console.error(err);
+                return;
+            }
+            const json = JSON.parse(data);
+            json.push(newNote);
+            fs.writeFile('db/db.json', JSON.stringify(json), (err) => err ? console.log(err) : console.log('note saved'))
+        })
+        // fs.appendFile('db/db.json', noteString, (err) => 
+        //     err ? console.log(err) : console.log('note saved'))
+
+        const response = {
+            status: 'success',
+            body: newNote,
+            };
+          
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
 });
 
 app.listen(PORT, () =>
