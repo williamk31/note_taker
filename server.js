324e3748f96467ae95c92fc22830516eacf1d11b
goofy foot <williamk31@gmail.com>
2024-03-07 11:26:14 -0700

    added GET requests for index.html and notes.html; added .gitignore file

--- server.js
+++ server.js
@@ -2,3 +2,16 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const PORT = 3001
+
+// middleware to serve static files
+app.use(express.static('public'));
+
+// GET request for notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
